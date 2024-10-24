053f56710a191d9c6bbd61d8b4ab5e9488e7cfd9
Aslan <aslanreza77@gmail.com>
2024-10-24 12:45:09 +0330

    Seperate Constants

---
+++ src/constants/nav.js
@@ -0,0 +1,29 @@
+import { CiHome } from "react-icons/ci";
+import { PiTelevision } from "react-icons/pi";
+import { RiMovie2Line } from "react-icons/ri";
+import { CiSearch } from "react-icons/ci";
+export const navigation = [
+  {
+    label: "TV",
+    href: "tv",
+    icon: <PiTelevision />,
+  },
+  {
+    label: "Movies",
+    href: "movies",
+    icon: <RiMovie2Line />,
+  },
+];
+export const mobileNav = [
+  {
+    label: "Home",
+    href: "/",
+    icon: <CiHome />,
+  },
+  ...navigation,
+  {
+    label: "Search",
+    href: "/search",
+    icon: <CiSearch />,
+  },
+];
