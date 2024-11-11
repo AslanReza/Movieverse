683f2cebd4c23310a7d5a6b68d73b71675a52982
Aslan <aslanreza77@gmail.com>
2024-11-11 10:18:56 +0330

    Add & Styling Components  Colored and Simple Divider

---
+++ src/components/ColoredDivider.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ColoredDivider = () => {
+  return (
+    <div
+      class="
+      container
+      w-full
+      h-1
+      rounded-sm
+      bg-gradient-to-r
+      from-blue-500
+      via-red-500
+      to-yellow-500
+      background-animate
+      ease-out
+    "
+    ></div>
+  );
+};
+
+export default ColoredDivider;
