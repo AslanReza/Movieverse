0e724c03bdccceb8e53f4be316d54512b81ab914
Aslan <aslanreza77@gmail.com>
2024-11-25 10:36:05 +0330

    Modify gitignore File

--- src/App.jsx
+++ src/App.jsx
@@ -41,8 +41,8 @@ function App() {
       <div className="min-h-[91.8vh]">
         <Outlet />
       </div>
-      {/* <Footer /> */}
-      {/* <MobileNav /> */}
+      <Footer />
+      <MobileNav />
     </main>
   );
 }
